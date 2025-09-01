be3b2d669097ba7f8e14a4b39c5a22527023d58b
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-09-01 17:51:47 +0900

    [fix]ホバー時の色を変更

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ children, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="rounded-lg bg-[#C73BA4] px-8 py-1.5 text-white font-bold hover:bg-pink-600 transition-colors"
+      className="rounded-lg bg-[#C73BA4] px-8 py-1.5 text-white font-bold hover:bg-[#F66FD4] transition-colors"
     >
       {children}
     </button>
