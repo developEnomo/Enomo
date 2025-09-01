9b9867367d5e9c8179f69124c03c6a6ed631e937
nakatashingo <235711nakatashingo@gmail.com>
2025-09-02 00:55:30 +0900

    [fix] loginページのコンポーネントの配置の修正

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/LoginForm';
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+    <div className="flex min-h-screen items-start justify-center bg-gray-50">
       <LoginForm />
     </div>
   );
