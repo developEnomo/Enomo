367056a7e5c7f6fbf699441bc9a9c6e18b115740
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-09-02 20:30:53 +0900

    [feat]ポップアップメニューを作成

--- web/src/components/footer/EnergyButton.tsx
+++ web/src/components/footer/EnergyButton.tsx
@@ -12,24 +12,36 @@ const EnergyButton = () => {
   const EnergyButtonClick = () => {
     setMenuOpen((prev) => !prev);
   };
-  return(
-  <button onClick={EnergyButtonClick}>
-    <Image
-      src={
-        menuOpen
-          ? "/Images/EnergyIcons/Energy_empty_push.svg"
-          : selected !== null
-          ? `/Images/EnergyIcons/Energy_fill_${selected}.svg`
-          : "/Images/EnergyIcons/Energy_empty.svg"
-      }
-      alt="energy"
-      width={100}
-      height={100}
-    />
-  </button>
-  
-
+  return (
+    <>
+      <button onClick={EnergyButtonClick}>
+        <Image
+          src={
+            menuOpen
+              ? "/Images/EnergyIcons/Energy_empty_push.svg"
+              : selected !== null
+              ? `/Images/EnergyIcons/Energy_fill_${selected}.svg`
+              : "/Images/EnergyIcons/Energy_empty.svg"
+          }
+          alt="energy"
+          width={100}
+          height={100}
+        />
+      </button>
+      {menuOpen && (
+        <div className="flex justify-center items-center border-2 border-[#C73BA4] px-4 py-2 m-4 bg-white rounded-lg">
+          <button>
+            {" "}
+            <Image
+              src="/Images/EnergyIcons/Face1.svg"
+              width={50}
+              height={50}
+              alt="face1"
+            />
+          </button>
+        </div>
+      )}
+    </>
   );
-  
 };
 export default EnergyButton;
--- web/src/components/footer/footer.tsx
+++ web/src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import EnergyButton from "./EnergyButton";
 const Footer = () => {
   return (
 
-      <div className="fixed bottom-0 w-full flex justify-center bg-black py-4">
+      <div className="fixed bottom-0 w-full flex flex-col-reverse items-center justify-center bg-black py-4">
        <EnergyButton/>
 </div>
   );
