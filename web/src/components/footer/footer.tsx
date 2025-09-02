a067d18b5a0ba0207a0f1d3eb694a8f1ba4cc1ab
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-09-02 19:18:02 +0900

    [feat]フッターのアイコンを画面下に固定

--- web/src/components/footer/footer.tsx
+++ web/src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import EnergyButton from "./EnergyButton";
 const Footer = () => {
   return (
 
-      <div className="fixed bottom-0 h-40 w-40 bg-[#000000] flex justify-center w-full flex justify-center">
+      <div className="fixed bottom-0 w-full flex justify-center bg-black py-4">
        <EnergyButton/>
 </div>
   );
