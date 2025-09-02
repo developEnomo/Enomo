5811e5fe1f4d12f740729f30edde4ac12ec039a8
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-09-03 00:43:17 +0900

    [add]各種画像インポート

--- web/src/components/footer/footer.tsx
+++ web/src/components/footer/footer.tsx
@@ -1,11 +1,9 @@
-
 import EnergyButton from "./EnergyButton";
 const Footer = () => {
   return (
-
-      <div className="fixed bottom-0 w-full flex flex-col-reverse items-center justify-center bg-black py-4">
-       <EnergyButton/>
-</div>
+    <div className="fixed bottom-0 w-full flex flex-col-reverse items-center justify-center py-4">
+      <EnergyButton />
+    </div>
   );
 };
 export default Footer;
