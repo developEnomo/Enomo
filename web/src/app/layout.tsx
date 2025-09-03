d153a5ad5816c3af15644e60a76910b80f9d31b4
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-09-04 01:30:49 +0900

    [fix]layoutにpb250px追加

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="pb-8">
+        <div className="pb-[250px]">
         {children}
         </div>
       </body>
