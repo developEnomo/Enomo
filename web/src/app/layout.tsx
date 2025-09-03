99f19503a96c61b5854c6272e89dfc900aa09249
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-09-03 23:38:37 +0900

    [fix]layoutにpb-8を追加

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="pb-8">
         {children}
+        </div>
       </body>
     </html>
   );
