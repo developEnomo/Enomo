241363cb1c87c78a82f13781f41f9e22f2be689a
FastDefence <aaa100@outlook.jp>
2025-09-03 17:37:45 +0900

    [fix]　使用APIを記録

--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -27,19 +27,19 @@ func New(
 	e.Use(middleware.Logger())
 
 	api := e.Group("/api/v1")
-	api.POST("/users/register", reg.Register)
-	api.POST("/users/login", login.Login)
-	api.POST("/users/logout", logout.Logout)
-	api.GET("/users/list", list.List)
-	api.PATCH("/users/rename", rename.Update)
-	api.PATCH("/users/energy", energy.Update)
-	api.POST("/users/delete", delete.Delete)
+	api.POST("/users/register", reg.Register) //
+	api.POST("/users/login", login.Login)     //
+	api.POST("/users/logout", logout.Logout)  //
+	api.GET("/users/list", list.List)         //
+	api.PATCH("/users/rename", rename.Update) //
+	api.PATCH("/users/energy", energy.Update) //
+	api.POST("/users/delete", delete.Delete)  //
 
-	api.POST("/groups/make", groupMake.Make)
-	api.POST("/groups/add", groupAdd.Add)
+	api.POST("/groups/make", groupMake.Make) //
+	api.POST("/groups/add", groupAdd.Add)    //
 	api.GET("/groups/list", groupList.List)
-	api.POST("/groups/delete", groupDelete.Delete)
-	api.POST("/groups/leave", groupLeave.Leave)
+	api.POST("/groups/delete", groupDelete.Delete) //
+	api.POST("/groups/leave", groupLeave.Leave)    //
 
 	api.GET("/groups/:groupId/recommendations", reco.Get)
 
