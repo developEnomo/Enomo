57faf911f3d1b22313d35469a100fb3c167a2715
screana <ryooya0906@gmail.com>
2025-09-04 10:47:50 +0900

    [fix] server -> api

--- api/internal/domain/group_settings_handler.go
+++ api/internal/domain/group_settings_handler.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"enomo/server/internal/usecase"
+	"enomo/api/internal/usecase"
 )
 
 type GroupSettingsHandler struct{ UC usecase.GroupSettingsUsecase }
