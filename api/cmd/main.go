82eb587051438c61c1e0b2b78ec2feec6d705d0b
FastDefence <aaa100@outlook.jp>
2025-09-05 00:37:33 +0900

    [fix] fix conflicts

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -68,7 +68,7 @@ func main() {
 	ulistH := domain.NewGroupListHandler(ulistUC)
 	gdelH := domain.NewGroupDeleteHandler(gdelUC, store)
 	leaveH := domain.NewGroupLeaveHandler(leaveUC, store)
-	recoH := domain.NewRecommendationsHandler(recoUC)
+	recoH := domain.NewRecommendationsHandler(recoUC, recoRefreshUC)
 	settingsH := domain.NewGroupSettingsHandler(settingsUC, store)
 
 	// --- router ---
