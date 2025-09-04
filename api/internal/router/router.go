3ecfa904f048257a400f9cc11f818967b1d66be5
screana <ryooya0906@gmail.com>
2025-09-04 10:37:23 +0900

    [feat] routerにハンドラーとエンドポイントの追加

--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -21,6 +21,8 @@ func New(
 	groupDelete *domain.GroupDeleteHandler,
 	groupLeave *domain.GroupLeaveHandler,
 	reco *domain.RecommendationsHandler,
+	groupSettings *domain.GroupSettingsHandler,
+	
 ) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Recover())
@@ -39,6 +41,8 @@ func New(
 	api.POST("/groups/add", groupAdd.Add)    //
 	api.GET("/groups/list", groupList.List)
 	api.POST("/groups/delete", groupDelete.Delete) //
+	api.GET("/groups/settings", groupSettings.Get) //
+	api.POST("/groups/settings/update", groupSettings.Update) //
 	api.POST("/groups/leave", groupLeave.Leave)    //
 
 	api.GET("/groups/:groupId/recommendations", reco.Get)
