33400e0f83ffceb727ceccf41e5dc539bb737eaa
screana <ryooya0906@gmail.com>
2025-09-04 17:17:03 +0900

    [feat] 作成したハンドラーの登録

--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -22,30 +22,34 @@ func New(
 	groupLeave *domain.GroupLeaveHandler,
 	reco *domain.RecommendationsHandler,
 	groupSettings *domain.GroupSettingsHandler,
-
 ) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 
 	api := e.Group("/api/v1")
-	api.POST("/users/register", reg.Register) //
-	api.POST("/users/login", login.Login)     //
-	api.POST("/users/logout", logout.Logout)  //
-	api.GET("/users/list", list.List)         //
-	api.PATCH("/users/rename", rename.Update) //
-	api.PATCH("/users/energy", energy.Update) //
-	api.POST("/users/delete", delete.Delete)  //
 
-	api.POST("/groups/make", groupMake.Make) //
-	api.POST("/groups/add", groupAdd.Add)    //
+	// users
+	api.POST("/users/register", reg.Register)
+	api.POST("/users/login", login.Login)
+	api.POST("/users/logout", logout.Logout)
+	api.GET("/users/list", list.List)
+	api.PATCH("/users/rename", rename.Update)
+	api.PATCH("/users/energy", energy.Update)
+	api.POST("/users/delete", delete.Delete)
+
+	// groups
+	api.POST("/groups/make", groupMake.Make)
+	api.POST("/groups/add", groupAdd.Add)
 	api.GET("/groups/list", groupList.List)
-	api.POST("/groups/delete", groupDelete.Delete) //
-	api.GET("/groups/settings", groupSettings.Get) //
+	api.POST("/groups/delete", groupDelete.Delete)
+	api.GET("/groups/settings", groupSettings.Get)
 	api.POST("/groups/settings/update", groupSettings.Update)
-	api.POST("/groups/leave", groupLeave.Leave)    //
+	api.POST("/groups/leave", groupLeave.Leave)
 
+	// recommendations
 	api.GET("/groups/:groupId/recommendations", reco.Get)
+	api.POST("/groups/:groupId/recommendations/refresh", reco.Refresh)
 
 	return e
 }
