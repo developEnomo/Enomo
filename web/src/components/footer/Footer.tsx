a14972e8ba6f8e3fbb86d066ec00380a836f973c
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-09-06 23:03:20 +0900

    [feaat]フッターの背景を追加

--- web/src/components/footer/Footer.tsx
+++ web/src/components/footer/Footer.tsx
@@ -1,8 +1,14 @@
 import EnergyButton from "./EnergyButton";
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full flex flex-col-reverse items-center justify-center py-4">
-      <EnergyButton />
+    <div className="fixed bottom-0 w-full h-[60px] z-10">
+      {/* 背景レイヤー */}
+      <div className="absolute bottom-0 w-full h-[60px] bg-[#C73BA4] z-0" />
+
+      {/* ボタンレイヤー */}
+      <div className="fixed bottom-0 w-full flex flex-col-reverse items-center justify-center py-4">
+        <EnergyButton />
+      </div>
     </div>
   );
 };
