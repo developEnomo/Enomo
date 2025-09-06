7c665dabe4d1d40b1d536ddc6cebe8720c8362c6
nakatashingo <235711nakatashingo@gmail.com>
2025-09-07 00:07:05 +0900

    [fix] 次の更新まで24時間以下の場合，～時間と表示されるように変更

--- web/src/components/UpdateCountdown.tsx
+++ web/src/components/UpdateCountdown.tsx
@@ -1,11 +1,18 @@
 type UpdateCountdownProps = {
-  daysLeft: number;
+  // `daysLeft` から `updateIntervalInHours` に変更
+  updateIntervalInHours: number;
 };
 
-export default function UpdateCountdown({ daysLeft }: UpdateCountdownProps) {
+export default function UpdateCountdown({ updateIntervalInHours }: UpdateCountdownProps) {
+  // 24時間以下であれば時間で表示、それ以上は日数で表示
+  const isWithin24Hours = updateIntervalInHours <= 24;
+  const displayText = isWithin24Hours
+    ? `${updateIntervalInHours} 時間`
+    : `${Math.ceil(updateIntervalInHours / 24)} 日`;
+
   return (
     <p className="text-right text-[#861F6D] font-semibold">
-      次の更新まで {daysLeft} 日
+      次の更新まで {displayText}
     </p>
   );
 }
